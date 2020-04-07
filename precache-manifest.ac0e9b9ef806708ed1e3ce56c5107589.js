9b3005bd6381124952de3f6c9323e86a9e9a0575
amamdouh1939 <ahmad.mohammed.1939@gmail.com>
2020-04-07 12:00:24 +0200

    Updates

---
+++ precache-manifest.ac0e9b9ef806708ed1e3ce56c5107589.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b5dd904a6caaf0d7773c3d3ba125c5",
+    "url": "/full-stack-10-reactjs-task-1/index.html"
+  },
+  {
+    "revision": "2bcaab07360e16ad598e",
+    "url": "/full-stack-10-reactjs-task-1/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "00f0a112028d7e0e0fa7",
+    "url": "/full-stack-10-reactjs-task-1/static/css/main.4498843b.chunk.css"
+  },
+  {
+    "revision": "2bcaab07360e16ad598e",
+    "url": "/full-stack-10-reactjs-task-1/static/js/2.f42171df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/full-stack-10-reactjs-task-1/static/js/2.f42171df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f0a112028d7e0e0fa7",
+    "url": "/full-stack-10-reactjs-task-1/static/js/main.a1c290a6.chunk.js"
+  },
+  {
+    "revision": "bf04b992fe2be325a85b",
+    "url": "/full-stack-10-reactjs-task-1/static/js/runtime-main.5180e54c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/full-stack-10-reactjs-task-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
